9406dfb2aca1aff612bc5abdb417b04152739dd1
Bruna Villanova <114350625+brunavillanova@users.noreply.github.com>
2026-04-07 12:16:48 -0300

    curriculo atualizado

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -60,7 +60,7 @@ const Hero = () => {
                             <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
                                     {/* Botão para Download do CV */}
-                                    <StyledButton downloadLink="/curriculo-2.pdf">
+                                    <StyledButton downloadLink="/brunavillanova_Curriculo.pdf">
                                         <DownloadIcon />
                                         <Typography>
                                             Download CV
